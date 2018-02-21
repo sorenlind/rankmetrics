6762f045746038b7e6d31443b6ff0a9d9371c9eb
Søren Lind Kristiansen <soren@gutsandglory.dk>
2018-02-21 16:36:35 +0100

    Update various configuration files

--- setup.py
+++ setup.py
@@ -1,31 +1,9 @@
 """Setup script for package."""
 import re
-from distutils.core import Command
 from setuptools import setup, find_packages
 
-
-class PyTest(Command):
-    """Setup class for pytest."""
-
-    user_options = []
-
-    def initialize_options(self):
-        pass
-
-    def finalize_options(self):
-        pass
-
-    def run(self):
-        """Run the tests."""
-        import subprocess
-        import sys
-        errno = subprocess.call([sys.executable, "runtests.py"])
-        raise SystemExit(errno)
-
-
 VERSION = re.search(r'^VERSION\s*=\s*"(.*)"', open("rankmetrics/version.py").read(), re.M).group(1)
-
-with open("./README.md", "rb") as f:
+with open("README.md", "rb") as f:
     LONG_DESCRIPTION = f.read().decode("utf-8")
 
 setup(
@@ -35,19 +13,20 @@ setup(
     long_description=LONG_DESCRIPTION,
     author="Soren Lind Kristiansen",
     author_email="soren@gutsandglory.dk",
+    url="https://github.com/sorenlind/rankmetrics/",
     keywords="recommender system learning to rank python",
-    platforms=["Any"],
     packages=find_packages(),
-    include_package_data=True,
-    zip_safe=False,
     install_requires=["numpy", "scipy", "sklearn"],
+    extras_require={
+        'test': ['pytest', 'tox', 'pandas'],
+    },
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest', 'pandas'],
     classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Environment :: Console",
-        "License :: OSI Approved :: Apache Software License",
-        "Operating System :: OS Independent",
-        'Programming Language :: Python :: 3.5',
-    ],
-    cmdclass={
-        "test": PyTest
-    }, )
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.6'
+    ])
